24060f72dc582619499dca7e833be21b753a42f7
Rodrigo Gryzinski <rogryza@gmail.com>
2020-01-27 08:24:21 -0300

    Auth tests are back

--- test/Effects/Auth.hs
+++ test/Effects/Auth.hs
@@ -6,21 +6,24 @@ module Effects.Auth
 where
 
 import Hedgehog
--- import qualified Hedgehog.Gen as Gen
--- import qualified Hedgehog.Range as Range
--- import PG.Types
--- import PG.Effects.Auth
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import PG.Types
+import PG.Effects.Auth
 
--- prop_htpasswd_roundtrip :: Property
--- prop_htpasswd_roundtrip = property $ do
---   namesAndPasswords <- forAll $ Gen.list (Range.linear 0 3) genPair
---   serialized <- writeHtpasswd (pure "0000000000000000") namesAndPasswords
---   mapM_ validatePassword $ zip namesAndPasswords (parseHtpasswd serialized)
---   where
---     genPair = (,) <$> Gen.element ["a", "username", "foobar"] <*> Gen.element ["b", "hunter123", "qux"]
---     validatePassword ((name, p), (User {userName}, h)) = do
---       name === userName
---       assert $ checkHtpasswd p h
+prop_htpasswd_roundtrip :: Property
+prop_htpasswd_roundtrip = property $ do
+  namesAndPasswords <- forAll $ Gen.list (Range.linear 0 3) genPair
+  serialized <- writeHtpasswd (pure "0000000000000000") namesAndPasswords
+  mapM_ (validatePassword' $ parseHtpasswd serialized) namesAndPasswords
+  where
+    genPair = (,) <$> Gen.element ["a", "username", "foobar"] <*> Gen.element ["b", "hunter123", "qux"]
+    validatePassword' htpasswd (name, p) = do
+      (User {userName}, hash) <- case fetchUser htpasswd name of
+        Just x -> return x 
+        Nothing -> failure
+      name === userName
+      assert $ validatePassword htpasswd p hash
 
 tests :: Group
 tests = $$(discover)
